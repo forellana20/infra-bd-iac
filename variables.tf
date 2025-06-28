669ded1f353f17d17f2a295e717f21d28bfc4f78
Fernando Orellana <ferorellan20@gmail.com>
2025-06-28 13:14:05 -0600

    agregando variables para providers

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "Id del proyecto para la ejecucion"
+  type        = string
+}
+
+variable "region" {
+  description = "Region donde se desplegaran los recursos"
+  type        = string
+  
+}
